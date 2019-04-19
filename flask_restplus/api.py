742e84683d236e56fc4f0214a7d2efaf58e73d2b
gody <benoit.godard@intech.lu>
2019-04-19 16:06:10 +0200

    Fixed a bug which was preventing to correctly register api documentation using same url prefix as the one used for the api when using blueprints
    (cherry picked from commit 945dd25bc147621659d3f346589fe79cedb3d7c2)

--- flask_restplus/api.py
+++ flask_restplus/api.py
@@ -190,7 +190,7 @@ class Api(object):
         else:
             self.blueprint = app
 
-    def _init_app(self, app):
+    def _init_app(self, app, url_prefix=None):
         '''
         Perform initialization actions with the given :class:`flask.Flask` object.
 
@@ -206,8 +206,8 @@ class Api(object):
             for resource, namespace, urls, kwargs in self.resources:
                 self._register_view(app, resource, namespace, *urls, **kwargs)
 
-        self._register_apidoc(app)
         self._validate = self._validate if self._validate is not None else app.config.get('RESTPLUS_VALIDATE', False)
+        self._register_apidoc(app, url_prefix=url_prefix)
         app.config.setdefault('RESTPLUS_MASK_HEADER', 'X-Fields')
         app.config.setdefault('RESTPLUS_MASK_SWAGGER', True)
 
@@ -229,10 +229,12 @@ class Api(object):
         parts = (registration_prefix, self.prefix, url_part)
         return ''.join(part for part in parts if part)
 
-    def _register_apidoc(self, app):
+    def _register_apidoc(self, app, url_prefix=None):
         conf = app.extensions.setdefault('restplus', {})
         if not conf.get('apidoc_registered', False):
-            app.register_blueprint(apidoc.apidoc)
+            if not url_prefix and self.blueprint:
+                url_prefix = self.blueprint.url_prefix
+            app.register_blueprint(apidoc.apidoc, url_prefix=url_prefix)
         conf['apidoc_registered'] = True
 
     def _register_specs(self, app_or_blueprint):
@@ -787,7 +789,7 @@ class Api(object):
                                                   setup_state)
         if not setup_state.first_registration:
             raise ValueError('flask-restplus blueprints can only be registered once.')
-        self._init_app(setup_state.app)
+        self._init_app(setup_state.app, url_prefix=setup_state.url_prefix)
 
     def mediatypes_method(self):
         '''Return a method that returns a list of mediatypes'''
